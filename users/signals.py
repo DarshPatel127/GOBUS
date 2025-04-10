e94f4a6c9cd22b41145362da1ec107bd721fac39
Darsh Patel <f20241338@pilani.bits-pilani.ac.in>
2025-04-10 18:16:19 +0530

    profile

--- users/signals.py
+++ users/signals.py
@@ -3,9 +3,10 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
 from exp.models import Wallet
+from CustomUser.models import CustomUser
 
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=CustomUser)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
