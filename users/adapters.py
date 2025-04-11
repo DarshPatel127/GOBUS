4c2f14171308b4612f9343bacf1c980a4e5f290b
Darsh Patel <f20241338@pilani.bits-pilani.ac.in>
2025-04-11 15:21:43 +0530

    revert

--- users/adapters.py
+++ users/adapters.py
@@ -2,18 +2,6 @@ from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from django.shortcuts import resolve_url
 
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
-    def is_open_for_signup(self, request, sociallogin):
-        # Always allow signup
-        return True
-
-    def save_user(self, request, sociallogin, form=None):
-        user = sociallogin.user
-        user.set_unusable_password()
-        user.email = sociallogin.account.extra_data.get('email', '')
-        user.is_busadmin = False 
-        user.save()
-        return user
-
     def get_login_redirect_url(self, request):
         return resolve_url('/profile/')
 
