cca32ec81eb4c1cb879f0af5028625c2e142c357
Darsh Patel <f20241338@pilani.bits-pilani.ac.in>
2025-04-22 23:58:04 +0530

    smtp service details in .env

--- P2/settings.py
+++ P2/settings.py
@@ -175,9 +175,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'darshpatel610@gmail.com'
-EMAIL_HOST_PASSWORD = 'dazmkivhxzczggug'
-
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
